51cbf4f94d05038fba981feee72abab3c9b5472b
vishnu-46 <vishnuvk2027@gmail.com>
2025-05-27 15:42:59 +0530

    build

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+             'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'django_db',
+            'USER': 'postgres',
+            'PASSWORD': '2002',
+            'HOST': 'localhost',
+            'PORT': '5432'
     }
 }
 
