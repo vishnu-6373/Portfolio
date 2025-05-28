84a7973f9197bf38a114ff375ca8cb6848295cd6
vishnu-46 <vishnuvk2027@gmail.com>
2025-05-28 22:38:11 +0530

    8

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,7 +7,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
 ]
 
 if settings.DEBUG:
