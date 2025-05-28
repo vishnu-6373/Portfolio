ec41f61edbbbe12fdc0885c9c2e6566d2fdeafbb
vishnu-46 <vishnuvk2027@gmail.com>
2025-05-28 19:30:41 +0530

    update favicon

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
 ]
 
 if settings.DEBUG:
